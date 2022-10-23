9c0a4ff0602100bdcae944bd6a36d97363b101cb
unknown <ahmedsamir96m@gmail.com>
2022-10-23 21:32:39 +0200

    install sanity for backend, created schemas pin, user, comment, save, postedBy -- install react js, tailwindcss for frontend

---
+++ blah_backend/schemas/pin.js
@@ -0,0 +1,61 @@
+export default {
+	name: 'pin',
+	title: 'Pin',
+	type: 'document',
+	fields: [
+		{
+			name: 'title',
+			title: 'Title',
+			type: 'string',
+		},
+		{
+			name: 'about',
+			title: 'About',
+			type: 'string',
+		},
+		{
+			name: 'image',
+			title: 'Image',
+			type: 'image',
+			options: {
+				hotspot: true,
+			}
+		},
+		{
+			name: 'destination',
+			title: 'Destination',
+			type: 'url',
+		},
+		{
+			name: 'category',
+			title: 'Category',
+			type: 'url',
+		},
+		{
+			name: 'userId',
+			title: 'UserId',
+			type: 'string',
+		},
+		{
+			name: 'postedBy',
+			title: 'PostedBy',
+			type: 'postedBy',
+		},
+		{
+			name: 'save',
+			title: 'Save',
+			type: 'array', 
+			of: [
+				{type: 'save'}
+			],
+		},
+		{
+			name: 'comments',
+			title: 'Comments',
+			type: 'array', 
+			of: [
+				{type: 'comment'}
+			],
+		},
+	]
+}
